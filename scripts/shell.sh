d9de2dbace75249c5e1d0146db297edca43a5cdc
Eric Steen <ericsteen1@gmail.com>
2019-10-11 08:34:11 -0700

    n

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -5,7 +5,16 @@
 #     docker/compose:1.24.0'"'
 # alias dcd='docker-compose down'
 # alias dcu='docker-compose up --build'
-# alias dcun='docker-compose build --no-cache; docker-compose up'
+# alias dkNin='docker network inspect'
+# alias dkNls='docker network ls'
+# alias dkc='docker-compose'
+# alias dkcb='docker-compose build'
+# alias dkcB='docker-compose build --no-cache'
+# alias dkcd='docker-compose down'
+# alias dkce='docker-compose exec -e COLUMNS=`tput cols` -e LINES=`tput lines`'
+# alias dkck='docker-compose kill'
+# alias dkcl='docker-compose logs'
+# alias dkcL='docker-compose logs -f'
 # alias rmc='cd ..; rm -rf cryptowise'
 # alias sadd='eval $(ssh-agent -s)'
 # alias pull='git clone --recurse-submodules git@github.com:upstarter/cryptowise.git'
@@ -14,4 +23,7 @@
 # }
 # alias dfe='docker ps -aqf "name="envoy"'
 # alias dfu='docker ps -aqf "name=api"'
+# kurl() {
+#   curl --cacert ./certs/platform-web.ai.pem https://$1/v1/topics -vk
+# }
 #" >> ~/.bashrc
